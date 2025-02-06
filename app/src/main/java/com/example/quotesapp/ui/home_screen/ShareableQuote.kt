7642bc2c2000aea6fca64d17902e660df40430f6
shalenmj <shalenmj@gmail.ocm>
2025-02-07 00:17:17 +0530

    - working on sharing quote in feature branch

--- app/src/main/java/com/example/quotesapp/ui/home_screen/ShareableQuote.kt
+++ app/src/main/java/com/example/quotesapp/ui/home_screen/ShareableQuote.kt
@@ -1,13 +1,9 @@
-package com.example.quotesapp.ui
+package com.example.quotesapp.ui.home_screen
 
-import android.content.Intent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -33,7 +29,6 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.quotesapp.R
 import com.example.quotesapp.domain.model.Quote
-import com.example.quotesapp.ui.home_screen.util.QuoteEvent
 import com.example.quotesapp.ui.theme.GIFont
 import com.example.quotesapp.ui.theme.customBlack
 import com.example.quotesapp.ui.theme.customGrey

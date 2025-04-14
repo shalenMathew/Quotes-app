80da3f29e421dbeaa9a977e0bb8248e69e9066c3
shalenmj <shalenmj@gmail.ocm>
2025-04-15 01:36:56 +0530

    - fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.quotesapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

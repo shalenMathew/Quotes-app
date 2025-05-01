6c044def9872b0d0240aec644ca833ed95b905fc
shalenmj <shalenmj@gmail.ocm>
2025-05-01 14:56:22 +0530

    - version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.example.quotesapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

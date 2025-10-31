a7d6cf1de0f2c18cda8512dc0efca56fdc511a10
Aditya More <aditya1875more@gmail.com>
2025-10-31 23:50:57 +0530

    minimal brown card theme

--- app/src/main/java/com/shalenmathew/quotesapp/util/Constants.kt
+++ app/src/main/java/com/shalenmathew/quotesapp/util/Constants.kt
@@ -24,7 +24,8 @@ object Constants {
     const val igorTheme = "igorTheme"
     const val ReminderTheme = "ReminderTheme"
     const val FliplingoesTheme = "FliplingoesTheme"
-    const val MinimalBlackTheme = "MinimalBlackTheme"
+        const val MinimalBlackTheme = "MinimalBlackTheme"
+    const val MinimalBrownTheme = "MinimalBrownTheme"
 
     const val TravelTheme = "TravelCardTheme"
 

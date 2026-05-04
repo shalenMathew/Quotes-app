741c07e0cb1957a54947e71fceeb82a1bc6d4e23
ADMIN <shalenmj@gmail.com>
2026-05-04 17:53:08 +0530

    fix: improved setting ui

--- app/src/main/java/com/shalenmathew/quotesapp/presentation/screens/settings_screen/troubleshoot/component/ToubleshootComponent.kt
+++ app/src/main/java/com/shalenmathew/quotesapp/presentation/screens/settings_screen/troubleshoot/component/ToubleshootComponent.kt
@@ -11,6 +11,9 @@ val troubleshootQuestions = listOf(
     TroubleshootItem("Why isn't the widget refreshing?", "Android limits widget updates to save battery. Ensure the app is not 'Battery Optimized' in app system settings."),
     TroubleshootItem("How to add the widget?", "Long press on your home screen, select 'Widgets', find Quotes App, and drag it to your screen."),
     TroubleshootItem("From where do the widget pull quotes from?", "The widget can pull quotes from three sources: online, your favorites, or your own custom entries. You can easily choose your preferred source in the settings."),
+    TroubleshootItem("About Backup & Restore data? ", "You can import & export ur data , but make sure while ur exporting and trying to change name, the name should end with '.json' or the import wont work "),
+    TroubleshootItem("About Notification Scheduling? ", "You can only choose to schedule between 2 options , either the app can notify u every couple of hours or just every day on the scheduled time"),
     TroubleshootItem("what is 'Custom Quote'? ", "Its a feature that allows you to create and save your own quotes."),
-    TroubleshootItem("what is the Prism Rainbow? ", "Its a prism rainbow \uD83D\uDDE3\uFE0F"),
+    TroubleshootItem("why the apps so good? ", "cause i made it \uD83D\uDDE3\uFE0F"),
+    TroubleshootItem("why did u made the app? ", "cause the apps so good \uD83D\uDDE3\uFE0F"),
 )

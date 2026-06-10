c49a0970da8896de45a42cdf7fc7fb664cb5c088
ADMIN <shalenmj@gmail.com>
2026-06-10 12:46:58 +0530

    widget: fixes

--- app/src/main/java/com/shalenmathew/quotesapp/presentation/screens/settings_screen/troubleshoot/component/ToubleshootComponent.kt
+++ app/src/main/java/com/shalenmathew/quotesapp/presentation/screens/settings_screen/troubleshoot/component/ToubleshootComponent.kt
@@ -7,9 +7,10 @@ data class TroubleshootItem(
 )
 
 val troubleshootQuestions = listOf(
-    TroubleshootItem("Notifications are not appearing", "Check if notification permissions are granted and  ensure the app is not 'Battery Optimized' in app system settings."),
+    TroubleshootItem("Notifications are not appearing", "Please ensure the app is not 'Battery Optimized' in app system settings and Check if notification permissions are granted."),
     TroubleshootItem("Why isn't the widget refreshing?", "Android limits widget updates to save battery. Ensure the app is not 'Battery Optimized' in app system settings."),
-    TroubleshootItem("How to add the widget?", "Long press on your home screen, select 'Widgets', find Quotes App, and drag it to your screen."),
+    TroubleshootItem("How to add the widget?", "Long press on quotes app and add widget."),
+    TroubleshootItem("My widget acting funny?", "If u ever feel like , ur widget is not refreshing or not pulling quotes from desired source, try to reboot the device, Android OS sometimes makes widget to act weird"),
     TroubleshootItem("From where do the widget pull quotes from?", "The widget can pull quotes from three sources: online, your favorites, or your own custom entries. You can easily choose your preferred source in the settings."),
     TroubleshootItem("About Backup & Restore data? ", "You can import & export ur data , but make sure while ur exporting and trying to change name, the name should end with '.json' or the import wont work "),
     TroubleshootItem("About Notification Scheduling? ", "You can only choose to schedule between 2 options , either the app can notify u every couple of hours or just every day on the scheduled time"),

bac7d1be8cdb4171b5963b6708fa2a01ba5594ba
ADMIN <shalenmj@gmail.com>
2026-08-14 22:44:14 +0530

    gradle version fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,20 +55,18 @@ android {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
